bd0cdbbd63cc304641947fd71783a4c6f40b426d
Adrian Minde <adrian_minde@live.de>
2025-01-08 13:33:07 +0100

    Restructure main entry point

--- puma/__main__.py
+++ puma/__main__.py
@@ -10,18 +10,19 @@ To learn how to use photovoltaic utility and manufacturing systems, see "puma --
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 
-import puma
+
+os.environ["NUMEXPR_MAX_THREADS"] = str(os.cpu_count())
 
 
-def _get_parser() -> ArgumentParser:
-    from puma import __version__
+def main() -> None:
+    import puma
 
     parser = ArgumentParser(description=__doc__, formatter_class=RawTextHelpFormatter)
-    parser.add_argument("-v", "--version", action="version", version="%(prog)s {version}".format(version=__version__))
+    parser.add_argument("-v", "--version", action="version", version=f"%(prog)s {puma.__version__}")
 
-    return parser
+    with puma.load(parser=parser) as application:
+        application.main()
 
 
 if __name__ == "__main__":
-    with puma.load(parser=_get_parser()) as application:
-        application.main()
+    main()
